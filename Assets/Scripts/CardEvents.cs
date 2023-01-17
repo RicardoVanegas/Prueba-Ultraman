7160466982726e7b8a16eee60041bf9b2c35c633
RicardoVanegas <a01634956@tec.mx>
2023-01-17 13:02:03 -0600

    por el momento solo funcionan los clicks en la carta 1 pero no tiene sonido ni se guardan los datos

---
+++ Assets/Scripts/CardEvents.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CardEvents : MonoBehaviour
+{
+
+    public Card card;
+    public GameObject panelName;
+    public TMP_Text cardName;
+    public TMP_Text rarity;
+
+    public GameObject panelAttribute;
+    public TMP_Text attribute;
+
+
+    public TMP_Text hp,
+                    cost,
+                    ATK;
+    public GameObject panelHP;
+
+  
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cardName.text = card.cardName;
+        rarity.text = card.rarityText;
+        attribute.text = card.attributeText;
+        hp.text = card.hp.ToString();
+        cost.text = card.cost.ToString();
+        ATK.text = card.atk.ToString();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && Input.mousePosition.x >= 117 && Input.mousePosition.x <= 408 && Input.mousePosition.y >= 64 && Input.mousePosition.y <= 514)
+        {
+            panelName.SetActive(true);
+            
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse1) && Input.mousePosition.x >= 117 && Input.mousePosition.x <= 408 && Input.mousePosition.y >= 64 && Input.mousePosition.y <= 514)
+        {
+            panelHP.SetActive(true);
+        }
+       
+        
+    }
+}
